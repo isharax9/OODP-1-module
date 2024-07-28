04b6c6d547e73e07c81d5791ad0f3011cbb6889d
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-07-28 11:26:55 +0530

    Singleton

--- src/Singleton/A.java
+++ src/Singleton/A.java
@@ -9,5 +9,27 @@ package Singleton;
  * @author isharaLakshitha
  */
 public class A {
-    
+
+    private static A a;
+
+    private A() {
+
+    }
+
+    public static A getA() {
+        if (a == null) {
+            a = new A();
+        }
+        return a;
+    }
+}
+
+class Test {
+
+    public static void main(String[] args) {
+        A a1 = A.getA();
+        A a2 = A.getA();
+
+        System.out.println("a1==a2");
+    }
 }
